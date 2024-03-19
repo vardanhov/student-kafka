e7f5da5b19fef7532d791339c08819ff84fc5919
Оганнисян Вардан <v.ogannisyan@razvitie-mo.ru>
2024-03-19 23:23:23 +0400

    First commit

--- src/main/java/com/student/kafka/service/JsonStoreService.java
+++ src/main/java/com/student/kafka/service/JsonStoreService.java
@@ -1,8 +1,11 @@
 package com.student.kafka.service;
 
+import com.student.kafka.consumer.KafkaConsumer;
 import com.student.kafka.dto.Student;
 import com.student.kafka.model.JsonEntity;
 import com.student.kafka.repo.JsonRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,6 +15,7 @@ import java.util.List;
 public class JsonStoreService {
     private JsonRepository jsonRepository;
 
+    private static final Logger logger = LoggerFactory.getLogger(KafkaConsumer.class);
     @Autowired
     public JsonStoreService(JsonRepository jsonRepository) {
         this.jsonRepository = jsonRepository;
@@ -21,5 +25,6 @@ public class JsonStoreService {
         JsonEntity jsonEntity = new JsonEntity();
         jsonEntity.setJson(message);
         jsonRepository.save(jsonEntity);
+        logger.info("Json message saved");
     }
 }
